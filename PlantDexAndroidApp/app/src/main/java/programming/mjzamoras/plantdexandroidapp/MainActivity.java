b8acf0917215bab2a8cb25af3b7d47097509553c
Michael Jay Zamoras <mjzamoras@gmail.com>
2019-11-01 20:00:43 +0800

    Setup PlantDex Android App

---
+++ PlantDexAndroidApp/app/src/main/java/programming/mjzamoras/plantdexandroidapp/MainActivity.java
@@ -0,0 +1,14 @@
+package programming.mjzamoras.plantdexandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
