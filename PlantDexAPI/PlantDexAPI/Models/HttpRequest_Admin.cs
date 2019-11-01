74de3a3af0f27b4a24853c60726fc067087d6889
Michael Jay Zamoras <mjzamoras@gmail.com>
2019-11-01 20:44:11 +0800

    Setup Admin Request Handlers

---
+++ PlantDexAPI/PlantDexAPI/Models/HttpRequest_Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlantDexAPI.Models
+{
+    public class HttpRequest_Admin
+    {
+        public string requestSource { get; set; }
+        public string requestCommand { get; set; }
+        public string requestIdentity { get; set; }
+        public List<Object> data { get; set; }
+    }
+}
---
+++ PlantDexAPI/PlantDexAPI/Models/RequestManager_Admin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlantDexAPI.Models
+{
+    public static class RequestManager_Admin
+    {
+    }
+}
